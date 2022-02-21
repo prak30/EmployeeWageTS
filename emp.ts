f3d0e7de771e660315c064f40caa3dced19980fe
pranav <prnvkatkar88@gmail.com>
2022-02-21 17:50:18 +0530

    checks if employee is present or absent

--- emp.ts
+++ emp.ts
@@ -1,1 +1,10 @@
 console.log("welcome to employee wage");
+
+//UC1
+const IS_ABSENT : number = 0;
+let empCheck : number = Math.floor(Math.random()*10)%2;
+if (empCheck == IS_ABSENT){
+    console.log("EMPLOYEE ABSENT");  
+}else{
+    console.log("EMPLOYEE PRESENT");
+}
