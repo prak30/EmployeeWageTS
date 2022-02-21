00049a2db29d562da585271dbef3acd2c1160942
pranav <prnvkatkar88@gmail.com>
2022-02-21 18:37:57 +0530

    calculates daily employee wage

--- emp.ts
+++ emp.ts
@@ -1,10 +1,13 @@
 console.log("welcome to employee wage");
 
-//UC1
 const IS_ABSENT : number = 0;
+const WAGE_PER_HOUR : number = 20;
+const FULL_DAY_HOUR :number = 8;
 let empCheck : number = Math.floor(Math.random()*10)%2;
 if (empCheck == IS_ABSENT){
     console.log("EMPLOYEE ABSENT");  
 }else{
     console.log("EMPLOYEE PRESENT");
+    let dailyWage : number = WAGE_PER_HOUR * FULL_DAY_HOUR
+    console.log("Daily wage is " +dailyWage);
 }
