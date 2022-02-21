66a0b705958473fe509ac5a3506cc8d6bb930591
pranav <prnvkatkar88@gmail.com>
2022-02-21 18:56:10 +0530

    added part time employee and wage

--- emp.ts
+++ emp.ts
@@ -3,11 +3,16 @@ console.log("welcome to employee wage");
 const IS_ABSENT : number = 0;
 const WAGE_PER_HOUR : number = 20;
 const FULL_DAY_HOUR :number = 8;
-let empCheck : number = Math.floor(Math.random()*10)%2;
+const PART_TIME_HOUR : number = 4;
+let empCheck : number = Math.floor(Math.random()*10)%3;
 if (empCheck == IS_ABSENT){
     console.log("EMPLOYEE ABSENT");  
+}else if(empCheck == 1){
+    console.log("FULL TIME EMPLOYEE PRESENT");
+    let dailyWage : number = WAGE_PER_HOUR * FULL_DAY_HOUR;
+    console.log("Daily wage of full timer = " +dailyWage);
 }else{
-    console.log("EMPLOYEE PRESENT");
-    let dailyWage : number = WAGE_PER_HOUR * FULL_DAY_HOUR
-    console.log("Daily wage is " +dailyWage);
+    console.log("PART TIME EMPLOYEE PRESENT");
+    let dailyWage : number = WAGE_PER_HOUR * PART_TIME_HOUR;
+    console.log("Daily wage of part timer = " +dailyWage);
 }
