6323445369ffa406cb4c47007ada040e47eefadd
pranav <prnvkatkar88@gmail.com>
2022-02-21 19:46:43 +0530

    switch case added

--- emp.ts
+++ emp.ts
@@ -5,14 +5,26 @@ const WAGE_PER_HOUR : number = 20;
 const FULL_DAY_HOUR :number = 8;
 const PART_TIME_HOUR : number = 4;
 let empCheck : number = Math.floor(Math.random()*10)%3;
-if (empCheck == IS_ABSENT){
-    console.log("EMPLOYEE ABSENT");  
-}else if(empCheck == 1){
-    console.log("FULL TIME EMPLOYEE PRESENT");
-    let dailyWage : number = WAGE_PER_HOUR * FULL_DAY_HOUR;
-    console.log("Daily wage of full timer = " +dailyWage);
-}else{
-    console.log("PART TIME EMPLOYEE PRESENT");
-    let dailyWage : number = WAGE_PER_HOUR * PART_TIME_HOUR;
-    console.log("Daily wage of part timer = " +dailyWage);
+console.log(empCheck);
+switch(empCheck){
+    case 0:{
+        console.log("EMPLOYEE ABSENT");
+        break;
+    }
+    case 1:{
+        console.log("FULL TIME EMPLOYEE PRESENT");
+        let dailyWage : number = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        console.log("Daily wage of full timer = " +dailyWage);
+        break;
+    }
+    case 2:{
+        console.log("PART TIME EMPLOYEE PRESENT");
+        let dailyWage : number = WAGE_PER_HOUR * PART_TIME_HOUR;
+        console.log("Daily wage of part timer = " +dailyWage);
+        break;
+    }
+    default:{
+        console.log("no employee found");
+        break;
+    }
 }
